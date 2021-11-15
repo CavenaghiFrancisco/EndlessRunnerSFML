1203427d8246e9b53fa3b890772c4eed4fc612d5
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-11-15 20:26:58 -0300

    Added game manager class.

--- SFML/Fire.cpp
+++ SFML/Fire.cpp
@@ -1,3 +1,14 @@
 #include "Fire.h"
 
+Fire::Fire(Vector2f position, RectangleShape collider) {
+}
 
+Fire::~Fire() {
+}
+
+void Fire::SetPosition(Vector2f position) {
+}
+
+Vector2f Fire::GetPosition() {
+    return Vector2f();
+}
